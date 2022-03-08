99a3f80560117b03a9a2af392c981dbd4093fd04
md-tanvir-mahtab <92093024+md-tanvir-mahtab@users.noreply.github.com>
2022-03-08 11:52:04 +0600

    Solve exercise 01 (Chapter 04) of page 106

---
+++ 04_chapter04/exercises_page106/exercise01.py
@@ -0,0 +1,34 @@
+"""
+Program: exercise01.py
+Author: Md. Tanvir Mahtab
+Date: 2022-03-08
+
+Problem:
+    Assume that the variable data refers to the string "myprogram.exe" . Write the
+    values of the following expressions:
+    a. data[2]
+    b. data[-1]
+    c. len(data)
+    d. data[0:8]
+
+Solution:
+    a. "p"
+    b. "e"
+    c. 13
+    d. "myprogra"
+"""
+
+# Initialize the variable
+data = "myprogram.exe"
+
+# Code for (a)
+print(data[2])
+
+# Code for (b)
+print(data[-1])
+
+# Code for (c)
+print(len(data))
+
+# Code for (d)
+print(data[0:8])
